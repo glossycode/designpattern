2cf73cdf22521c55c2be29652c7466f36d6398dc
Olivier Champion <glossycode@me.com>
2019-11-25 08:01:48 +0100

    ...

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitLog4Net();
+            log4net.LogManager.GetLogger(typeof(Program)).Info("Test log");
+        }
+
+        private static void InitLog4Net()
+        {
+            log4net.Config.XmlConfigurator.Configure();            
+        }
+    }
+}
